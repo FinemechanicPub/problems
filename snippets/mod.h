843f4c0eda7b649f1aced219d1dc5e76cf33cbfc
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2025-07-05 04:29:33 +0300

    Fix typo

--- snippets/mod.h
+++ snippets/mod.h
@@ -22,7 +22,7 @@ T inv_mod(T x, T mod){
 /// @param a делимое
 /// @param b делитель
 /// @param mod модуль
-/// @return эквиалент (a / b) % mod
+/// @return эквивалент (a / b) % mod
 template<class T = long long>
 T div_mod(T a, T b, T mod) {
     return a * inv_mod(b, mod) % mod;
