110654fb92b226e48819fb34135881bf14754f55
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2025-07-11 12:39:23 +0300

    Add modulo inverse to comb_mod

--- snippets/combmod.py
+++ snippets/combmod.py
@@ -8,11 +8,15 @@ def comb_mod_frac(n: int, k: int, mod: int) -> tuple[int, int]:
     return numerator, denominator
 
 
-def comb_mod(n: int, k: int, mod: int) -> int:
+def comb_mod(n: int, k: int, mod: int, inv: bool = False) -> int:
     """
     Вычисление числа сочетаний по модулю
     >>> import math; comb_mod(5, 3, 7) == math.comb(5, 3) % 7
     True
+    >>> import math; comb_mod(5, 3, 7, True) * math.comb(5, 3) % 7
+    1
     """
     numerator, denominator = comb_mod_frac(n, k, mod)
+    if inv:
+        numerator, denominator = denominator, numerator
     return pow(denominator, mod - 2, mod) * numerator % mod
